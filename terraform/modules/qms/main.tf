83da45c2d8b313ead21678a47c8900d635edd4ef
Anuradha Bajpai <anuradhabajpai@google.com>
2023-01-09 09:27:21 -0800

    feat: adding new cloudFunction and cloudScheduler to configure app level alerts

--- terraform/modules/qms/main.tf
+++ terraform/modules/qms/main.tf
@@ -67,7 +67,7 @@ resource "google_pubsub_topic" "topic_alert_notification" {
   depends_on = [module.project-services]
 }
 
-# Cloud scheduler job to invoke cloud function
+# Cloud scheduler job to invoke list projects cloud function
 resource "google_cloud_scheduler_job" "job" {
   name             = var.scheduler_cron_job_name
   description      = var.scheduler_cron_job_description
@@ -84,6 +84,29 @@ resource "google_cloud_scheduler_job" "job" {
     http_method = "POST"
     uri         = google_cloudfunctions_function.function-listProjects.https_trigger_url
     body        = base64encode("{\"organizations\":\"${var.organizations}\",\"threshold\":\"${var.threshold}\",\"projectId\":\"${var.project_id}\"}")
+
+    oidc_token {
+      service_account_email = var.service_account_email
+    }
+  }
+}
+
+# Cloud scheduler job to invoke config app alert cloud function
+resource "google_cloud_scheduler_job" "job" {
+  name             = var.scheduler_cron_job_name
+  description      = var.scheduler_cron_job_description
+  schedule         = var.scheduler_cron_job_frequency
+  time_zone        = var.scheduler_cron_job_timezone
+  attempt_deadline = var.scheduler_cron_job_deadline
+  region           = local.expanded_region
+  depends_on       = [module.project-services]
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = google_cloudfunctions_function.function-configureAppAlert.https_trigger_url
 
     oidc_token {
       service_account_email = var.service_account_email
@@ -256,6 +279,8 @@ resource "google_cloudfunctions_function" "function-notificationProject" {
     HOME_PROJECT  = var.project_id
     ALERT_DATASET = var.big_query_alert_dataset_id
     ALERT_TABLE   = var.big_query_alert_table_id
+    APP_ALERT_DATASET = var.big_query_alert_dataset_id
+    APP_ALERT_TABLE   = var.big_query_app_alert_table_id
   }
 }
 
@@ -264,6 +289,41 @@ resource "google_cloudfunctions_function_iam_member" "invoker-notificationProjec
   project        = google_cloudfunctions_function.function-notificationProject.project
   region         = google_cloudfunctions_function.function-notificationProject.region
   cloud_function = google_cloudfunctions_function.function-notificationProject.name
+  depends_on     = [module.project-services]
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "serviceAccount:${var.service_account_email}"
+}
+
+# cloud function to configure app alerting
+resource "google_cloudfunctions_function" "function-configureAppAlert" {
+  name        = var.cloud_function_config_app_alert
+  description = var.cloud_function_config_app_alert_desc
+  runtime     = "java11"
+  region      = local.expanded_region
+
+  available_memory_mb   = var.cloud_function_config_app_alert_memory
+  source_archive_bucket = google_storage_bucket.bucket_gcf_source.name
+  source_archive_object = google_storage_bucket_object.source_code_notification_object.name
+  trigger_http          = true
+  entry_point           = "functions.ConfigureAppAlert"
+  service_account_email = var.service_account_email
+  timeout               = var.cloud_function_config_app_alert_timeout
+  depends_on            = [module.project-services]
+
+  environment_variables = {
+    HOME_PROJECT  = var.project_id
+    APP_ALERT_DATASET = var.big_query_alert_dataset_id
+    APP_ALERT_TABLE   = var.big_query_app_alert_table_id
+    CSV_SOURCE_URI = "gs://${google_storage_bucket.bucket_gcf_source.name}/${var.app_alert_csv_file_name}"
+  }
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker-configureAppAlert" {
+  project        = google_cloudfunctions_function.function-configureAppAlert.project
+  region         = google_cloudfunctions_function.function-configureAppAlert.region
+  cloud_function = google_cloudfunctions_function.function-configureAppAlert.name
   depends_on     = [module.project-services]
 
   role   = "roles/cloudfunctions.invoker"
@@ -390,6 +450,69 @@ resource "google_bigquery_dataset" "quota_usage_alert_dataset" {
   location      = var.big_query_dataset_location
   depends_on    = [module.project-services]
 }
+
+# BigQuery Table for decentralize (App) alerting
+resource "google_bigquery_table" "quota_monitoring_app_alerting_table" {
+  dataset_id = var.big_query_alert_dataset_id
+  table_id   = var.big_query_app_alert_table_id
+
+  time_partitioning {
+    type = var.big_query_table_partition
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "project_id",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "project id"
+  },
+  {
+    "name": "email_id",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "email id"
+  },
+  {
+    "name": "app_code",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "application code - by default incremental numbers"
+  },
+  {
+    "name": "dashboard_url",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "dashboard url"
+  },
+  {
+    "name": "custom_log_metric_id",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "custom log metric id"
+  },
+  {
+    "name": "notification_channel_id",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "notification channel id"
+  },
+  {
+    "name": "alert_policy_id",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "alert policy id"
+  }
+]
+EOF
+
+}
+
 
 
 # Custom log-based metric to send quota alert data through
