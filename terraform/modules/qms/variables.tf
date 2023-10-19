fb747f5d39747fa0eb6b47687b219a879d57e6d0
admin_ <admin@yannipeng.altostrat.com>
2023-10-19 05:11:51 +0000

    added variables

--- terraform/modules/qms/variables.tf
+++ terraform/modules/qms/variables.tf
@@ -109,7 +109,7 @@ variable "scheduler_app_alert_job_frequency" {
 variable "source_code_base_url" {
   description = "Base URL to use when downloading source code for QMS Cloud Functions"
   type        = string
-  default     = "https://github.com/ypenn21/quota-monitoring-solution/releases/download/"
+  default     = "https://github.com/google/quota-monitoring-solution/releases/download/"
 }
 
 variable "source_code_zip" {
